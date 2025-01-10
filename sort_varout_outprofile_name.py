6d8adf2f8eb500f5fd6dc7138605b4b32ff4309f
eleanorgb <eleanor.burke@metoffice.gov.uk>
2025-01-10 11:28:06 +0000

    fixing npp in imogen

--- sort_varout_outprofile_name.py
+++ sort_varout_outprofile_name.py
@@ -11,62 +11,67 @@ def sort_varout_outprofile_name(var):
     get name for output
     '''
     varout = var
-    if "npp" in var and "_nlim" in var:
-        varout = "npp"  #npp is different in Nitrogen/non-Nitrogen cases
-        if "tiles" in var:
-            varout = varout+"_tiles"
-        elif "pft" in var:
-            varout = varout+"-pft"
-        elif "total" in var:
-            varout = varout+"-total"
-    elif "npp" in var:
-        varout = "npp_nonlimitation"
-        if "pft" in var:
-            varout = varout+"-pft"
-        elif "total" in var:
-            varout = varout+"-total"
-    if "ecoatmflux" in var and "_nlim" in var and "isimip" in MIPNAME.lower():
-        varout = "ecoatmflux"  #npp is different in Nitrogen/non-Nitrogen cases
-    elif "ecoatmflux" in var and "isimip" in MIPNAME.lower():
-        varout = "ecoatmflux_nonlimitation"  #npp different in N/non-N cases
-    if "nbp" in var and "_nlim" in var and "isimip" in MIPNAME.lower():
-        varout = "nbp"  #npp is different in Nitrogen/non-Nitrogen cases
-    elif "nbp" in var and "isimip" in MIPNAME.lower():
-        varout = "nbp_nonlimitation"  #npp different in N/non-N cases
-    if "nbp" in var and "_nlim" in var and "cmip" in MIPNAME.lower():
-        varout = "nbp"  # npp different in N/non-N cases
-        if "pft" in var:
-            varout = varout+"-pft"
-        elif "total" in var:
-            varout = varout+"-total"
-    elif "nbp" in var and "cmip" in MIPNAME.lower():
-        varout = "nbp_nonlimitation"   # npp different in N/non-N cases
-        if "pft" in var:
-            varout = varout+"-pft"
-        elif "total" in var:
-            varout = varout+"-total"
-    outprofile = "monthly"
-    if "daily" in var:
-        varout = sub("_daily$", "", var)
-        outprofile = "daily"
-    if "annual" in var:
-        outprofile = "annual"
-        if not "npp" in varout:
-            varout = sub("_annual$", "", var)
-        if "npp_nlim" in var:
-            varout = "npp"
-            if "pft" in var:
-                varout = varout+"-pft"
-            elif "tiles" in var:
+    if "isimip" in MIPNAME.lower():
+        if "npp" in var and "_nlim" in var:
+            varout = "npp"  #npp is different in Nitrogen/non-Nitrogen cases
+            if "tiles" in var:
                 varout = varout+"_tiles"
+            elif "pft" in var:
+                varout = varout+"-pft"
             elif "total" in var:
                 varout = varout+"-total"
         elif "npp" in var:
             varout = "npp_nonlimitation"
             if "pft" in var:
                 varout = varout+"-pft"
-            elif "tiles" in var:
-                varout = varout+"_tiles"
             elif "total" in var:
                 varout = varout+"-total"
+        if "ecoatmflux" in var and "_nlim" in var:
+            varout = "ecoatmflux"  #npp is different in Nitrogen/non-Nitrogen cases
+        elif "ecoatmflux" in var:
+            varout = "ecoatmflux_nonlimitation"  #npp different in N/non-N cases
+        if "nbp" in var and "_nlim" in var:
+            varout = "nbp"  #npp is different in Nitrogen/non-Nitrogen cases
+        elif "nbp" in var:
+            varout = "nbp_nonlimitation"  #npp different in N/non-N cases
+    elif "cmip" in MIPNAME.lower() and "imogen" not in MIPNAME.lower():
+        if "nbp" in var and "_nlim" in var:
+            varout = "nbp"  # npp different in N/non-N cases
+            if "pft" in var:
+                varout = varout+"-pft"
+            elif "total" in var:
+                varout = varout+"-total"
+        elif "nbp" in var:
+            varout = "nbp_nonlimitation"   # npp different in N/non-N cases
+            if "pft" in var:
+                varout = varout+"-pft"
+            elif "total" in var:
+                varout = varout+"-total"
+    outprofile = "monthly"
+    if "daily" in var:
+        varout = sub("_daily$", "", var)
+        outprofile = "daily"
+    if "annual" in var:   # still not right for all npps
+        outprofile = "annual"
+        if "imogen" not in MIPNAME.lower():
+            if not "npp" in varout:
+                varout = sub("_annual$", "", var)
+            if "npp_nlim" in var:
+                varout = "npp"
+                if "pft" in var:
+                    varout = varout+"-pft"
+                elif "tiles" in var:
+                    varout = varout+"_tiles"
+                elif "total" in var:
+                    varout = varout+"-total"
+            elif "npp" in var:
+                varout = "npp_nonlimitation"
+                if "pft" in var:
+                    varout = varout+"-pft"
+                elif "tiles" in var:
+                    varout = varout+"_tiles"
+                elif "total" in var:
+                    varout = varout+"-total"
+        else:
+            varout = sub("_annual$", "", var)
     return varout, outprofile
