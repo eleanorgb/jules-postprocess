f79000d95b8339a61e118dcfbd0a023e6cf7e23c
Katty Huang <katty.huang@metoffice.gov.uk>
2026-02-03 16:09:41 +0000

    bugfix varout

--- sort_varout_outprofile_name.py
+++ sort_varout_outprofile_name.py
@@ -47,15 +47,11 @@ def sort_varout_outprofile_name(var):
     elif "cmip" in MIPNAME.lower() and "imogen" not in MIPNAME.lower():
         if "nbp" in var and "_nlim" in var:
             varout = "nbp"  # npp different in N/non-N cases
-            if "pft" in var:
-                varout = varout + "-pft"
-            elif "total" in var:
+            if "total" in var:
                 varout = varout + "-total"
         elif "nbp" in var:
             varout = "nbp_nonlimitation"  # npp different in N/non-N cases
-            if "pft" in var:
-                varout = varout + "-pft"
-            elif "total" in var:
+            if "total" in var:
                 varout = varout + "-total"
     outprofile = "monthly"
     if "daily" in var:
@@ -66,7 +62,7 @@ def sort_varout_outprofile_name(var):
         if "imogen" not in MIPNAME.lower():
             if not "npp" in varout:
                 varout = sub("_annual$", "", var)
-            if "npp_nlim" in var:
+            if "npp" in var and "_nlim" in var:
                 varout = "npp"
                 if "pft" in var:
                     varout = varout + "-pft"
